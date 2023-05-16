3fd6acc818ae8182269cfc198a27361ebfff8fab
angel esteban <angel.alderete17@tectijuana.edu.mx>
2023-05-16 15:47:44 -0700

    mapaup

--- Login_html_css-main/home.php
+++ Login_html_css-main/home.php
@@ -17,128 +17,118 @@
     <link rel="stylesheet" href="css/burgerMenu.css">
 </head>
 
-<body>
-    <!-- clase contenedor para el maps-->
-<?php
-include 'navigation.php';
-?>
+   <body>
+      <!-- clase contenedor para el maps-->
+                      <?php
+                      include 'navigation.php';
+                                   ?>
+                    <!-- insercion del maps -->
+                    <div class ="contenedorMapa"> <div id="map"></div>
+                     <script>
+                          mapboxgl.accessToken = 'pk.eyJ1IjoibWFkYXJhYWxhbiIsImEiOiJjbGdqdzhzdXYwMHV4M2VxNXZiODV4c3VzIn0.doXQbG8IWzpjubw0hj1pDA';
+                            var map = new mapboxgl.Map({
+                              container: 'map',
+                              style: 'mapbox://styles/mapbox/streets-v11'
+                                });
 
-<div class="contenedorHome">
+                                  map.addControl(new mapboxgl.NavigationControl());
+                                     </script>
 
-         <!-- insercion del maps -->
-    <div class="contenedorMapa">
-        <div id="map"></div>
-        <script>
-            mapboxgl.accessToken = 'pk.eyJ1IjoibWFkYXJhYWxhbiIsImEiOiJjbGdqdzhzdXYwMHV4M2VxNXZiODV4c3VzIn0.doXQbG8IWzpjubw0hj1pDA';
-            var map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11'
-            });
+<form class="Filtros">
+<div class="texto-Filtros">
+   <!-- filtro para la especie -->
+      <label>Especie</label> <br>
+      <select id="selectEspecie" class="select-box">
+          <option value="">Elige una opcion</option>
 
-            map.addControl(new mapboxgl.NavigationControl());
-        </script>
+          <?php           
+              include 'homeCallSelectOptions.php'; 
+          ?>
 
-    </div>
-     <!-- division para los filtros-->
-        <form class="Filtros">
-           
-        <div class="texto-Filtros">
-             <!-- filtro para la especie -->
-                <label>Especie</label> <br>
-                <select id="selectEspecie" class="select-box">
-                    <option value="">Elige una opcion</option>
+      </select><br>
 
-                    <?php           
-                        include 'homeCallSelectOptions.php'; 
-                    ?>
+      <!-- filtro para la raza -->
+      <label>Raza</label> <br>
+      <select id="selectRaza" class="select-box" disabled>
+      <option value="">Elige una opcion</option>
 
-                </select><br>
+      <!-- Opciones estan en el javascript-->
 
-                <!-- filtro para la raza -->
-                <label>Raza</label> <br>
-                <select id="selectRaza" class="select-box" disabled>
-                <option value="">Elige una opcion</option>
+      </select><br>
 
-                <!-- Opciones estan en el javascript-->
+      <!-- filtro para el sexo -->
+      <label>Sexo</label> <br>
+      <select class="select-box">
+      <option value="">Elige una opcion</option>
+          <option value="">Macho </option>
+          <option value="">Hembra</option>
+      </select><br>
 
-                </select><br>
+      <!-- filtro para la situacion -->
+      <label>Situacion</label> <br>
+      <select class="select-box">
+      <option value="">Elige una opcion</option>
+          <option value="">callejero </option>
+          <option value="">hogar</option>
+      </select><br>
+      
+      <!-- filtro para la edad -->
+      <label>Edad</label> <br>
+      <select class="select-box">
+      <option value="">Elige una opcion</option>
+          <option value="">cachorro lactante </option>
+          <option value="">cachorro destetado</option>
+          <option value="">Adolescente</option>
+          <option value="">Adulto</option>
+          <option value="">Anciano</option>
+       </select><br>
+      
+       <!-- filtro para la salud -->
+       <label>Problemas de salud</label> <br>
+       <select class="select-box">
+       <option value="">Elige una opcion</option>
+          <option value="">Sarna</option>
+          <option value="">Garrapatas</option>
+          <option value="">Pulgas</option>
+          <option value="">Sange en heces/orina</option>
+          <option value="">Diarrea</option>
+          <option value="">Vomito</option>
+          <option value="">Rabia</option>
+          <option value="">Alergias</option>
+          <option value="">Heridas infectadas</option>
+          <option value="">infeccion en ojos</option>
+          <option value="">moquillo</option>
+          <option value="">Parvo virus</option>
+          <option value="">Otro</option>
+       </select><br>
+      
 
-                <!-- filtro para el sexo -->
-                <label>Sexo</label> <br>
-                <select class="select-box">
-                <option value="">Elige una opcion</option>
-                    <option value="">Macho </option>
-                    <option value="">Hembra</option>
-                </select><br>
+       <!-- filtro para las heridas -->
+       <label>Heridas</label> <br>
+       <select class="select-box">
+       <option value="">Elige una opcion</option>
+          <option value="">Signos de maltrato </option>
+          <option value="">Atropellado</option>
+          <option value="">Patas lecionadas</option>
+          <option value="">laceraciones</option>
+          <option value="">Mordidas</option>
+          <option value="">Rasguños</option>
+          <option value="">Contuciones</option>
+          <option value="">Otros</option>
+          </select><br>
 
-                <!-- filtro para la situacion -->
-                <label>Situacion</label> <br>
-                <select class="select-box">
-                <option value="">Elige una opcion</option>
-                    <option value="">callejero </option>
-                    <option value="">hogar</option>
-                </select><br>
-                
-                <!-- filtro para la edad -->
-                <label>Edad</label> <br>
-                <select class="select-box">
-                <option value="">Elige una opcion</option>
-                    <option value="">cachorro lactante </option>
-                    <option value="">cachorro destetado</option>
-                    <option value="">Adolescente</option>
-                    <option value="">Adulto</option>
-                    <option value="">Anciano</option>
-                </select><br>
-                
-                <!-- filtro para la salud -->
-                <label>Problemas de salud</label> <br>
-                <select class="select-box">
-                <option value="">Elige una opcion</option>
-                    <option value="">Sarna</option>
-                    <option value="">Garrapatas</option>
-                    <option value="">Pulgas</option>
-                    <option value="">Sange en heces/orina</option>
-                    <option value="">Diarrea</option>
-                    <option value="">Vomito</option>
-                    <option value="">Rabia</option>
-                    <option value="">Alergias</option>
-                    <option value="">Heridas infectadas</option>
-                    <option value="">infeccion en ojos</option>
-                    <option value="">moquillo</option>
-                    <option value="">Parvo virus</option>
-                    <option value="">Otro</option>
-                </select><br>
-                
-
-                <!-- filtro para las heridas -->
-                <label>Heridas</label> <br>
-                <select class="select-box">
-                <option value="">Elige una opcion</option>
-                    <option value="">Signos de maltrato </option>
-                    <option value="">Atropellado</option>
-                    <option value="">Patas lecionadas</option>
-                    <option value="">laceraciones</option>
-                    <option value="">Mordidas</option>
-                    <option value="">Rasguños</option>
-                    <option value="">Contuciones</option>
-                    <option value="">Otros</option>
-                </select><br>
-
-                <!-- filtro para el comportamiento -->
-                <label>Comportamiento</label> <br>
-                <select class="select-box">
-                <option value="">Elige una opcion</option>
-                    <option value="">Docil</option>
-                    <option value="">Agresivo</option>
-                    <option value="">Miedo</option>
-                </select><br>
-                
-            </div>
-            
-        </form>
-    </div>
+       <!-- filtro para el comportamiento -->
+       <label>Comportamiento</label> <br>
+       <select class="select-box">
+       <option value="">Elige una opcion</option>
+          <option value="">Docil</option>
+          <option value="">Agresivo</option>
+          <option value="">Miedo</option>
+       </select><br>
+      
+      </div>
+      </div>
+</form>
 <script src="js/mainHome.js"></script>
-
 </body>
-
 </html>
