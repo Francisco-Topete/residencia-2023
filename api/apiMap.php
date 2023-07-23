c7b59b34cf938c98675d4a8307d8d39bc58b887e
Francisco-Topete <71350426+Francisco-Topete@users.noreply.github.com>
2023-07-23 16:19:02 -0700

    apiMap

--- api/apiMap.php
+++ api/apiMap.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(0);
 $json = file_get_contents('php://input');
 include '../sqlservercall.php';
 
@@ -8,7 +7,7 @@ $params = array($data['fotografia'],$data['especie'],$data['raza'],$data['situac
 $data['problemas_salud'],$data['heridas'],$data['comportamiento'],$data['locacion_latitud'],$data['locacion_longitud'],
 $data['fecha_registro'],$data['hora_registro']); 
 
-$query = sqlsrv_query( $conn, "EXEC dbo.RegistrarAnimalMapa @ID_Animal = 0, @Foto = ?, 
+$query= sqlsrv_query( $conn, "EXEC dbo.RegistrarAnimalMapa @ID_Animal = 0, @Foto = ?, 
 @Especie = ?, @Raza = ?, @Situacion = ?, @Edad = ?, @Problema_Salud = ?,
-@Herida = ?, @Agresividad = ?, @Latitud = ?, @Longitud = ?, @Fecha = ?, @Hora = ?;",$params);                                                                                                                                                       
+@Herida = ?, @Agresividad = ?, @Latitud = ?, @Longitud = ?, @Fecha = ?, @Hora = ?;",$params);     
 ?>
